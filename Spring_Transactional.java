7a3425b1e130539a8a5eb751bf0763d75fa5b061
Jahid Quraishi <68467966+git-zaidism@users.noreply.github.com>
2022-11-22 19:17:06 +0530

    Create Spring_Transactional.java
    The best way to use the Spring Transactional annotation

---
+++ Spring_Transactional.java
@@ -0,0 +1,25 @@
+
+/* The best way to use the Spring Transactional annotation */
+
+@Service
+@Transactional(readOnly = true)
+public class Userservice implements UserDetailsService
+
+@Override
+public UserDetails loadUserByUsername (String username)
+throws UsernameNot FoundException {
+...
+}
+
+
+@Transactional
+public void createUser (User user) {
+...
+}
+
+----------------------------------------------------------------------------------------------------------------------------
+/* The loadUserByUsername uses a read-only transaction, and since we are using Hibernate, Spring performs
+some read-only optimizations as wel.
+On the other hand, the createUser has to write to the database. Hence, it overrides the readonly attribute
+value with the default setting given by the @Transactional annotation, which is readOnly=false, therefore */
+----------------------------------------------------------------------------------------------------------------------------
